caadfda10bb5bcbd24463ab2416d0b8d73809aa3
amrihakim <amrihakim12@gmail.com>
2024-08-16 21:12:39 +0700

    update

--- main.go
+++ main.go
@@ -5,16 +5,13 @@ import (
 )
 
 func main() {
-	// Inisialisasi string
 	str := "selamat malam"
 
-	// Inisialisasi map untuk menghitung frekuensi kemunculan setiap karakter
 	charCount := make(map[string]int)
 
-	// Loop untuk memecah string menjadi karakter dan menghitung frekuensinya
 	for _, char := range str {
-		fmt.Println(string(char)) // Mencetak karakter per baris
-		charCount[string(char)]++ // Menambahkan frekuensi karakter ke dalam map
+		fmt.Println(string(char)) 
+		charCount[string(char)]++
 	}
 
 	// Mencetak hasil map
